750d7e7269d7a8866bc354b75aa2b681dea32831
mdadul <emdadulislam580@gmail.com>
2023-07-05 06:55:20 +0600

    [Refactor] Optimize code in Users::OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
-    def google_oauth2
-        # You need to implement the method below in your model (e.g. app/models/user.rb)
-        @user = User.from_omniauth(request.env['omniauth.auth'])
-  
-        if @user.persisted? # If user exists
-          flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google' # Show success message
-          sign_in_and_redirect @user, event: :authentication # Sign in user
-        else
-          session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-          redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-        end
-    end
-
-    def github
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted? # If user exists
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github' # Show success message
-        sign_in_and_redirect @user, event: :authentication # Sign in user
-      else
-        session['devise.github_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+  def google_oauth2
+    handle_omniauth('Google')
   end
-  
+
+  def github
+    handle_omniauth('Github')
+  end
+
+  private
+
+  def handle_omniauth(provider)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted? # If user exists
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: provider
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.#{provider.downcase}_data"] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
 end
