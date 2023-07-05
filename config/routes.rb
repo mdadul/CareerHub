e6ef30418993c8d0a05eb8cb5433db2eac683e77
mdadul <emdadulislam580@gmail.com>
2023-07-05 06:35:42 +0600

    [FEAT] oAuth using Google

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   root 'home#index'
 
 end
